2f72ae1f416eebdfdc01fc0e2da5ba80e9d8e3dd
NoachDev <noachdev@proton.me>
2026-03-08 11:30:05 -0300

    feat: add audio listener ( playback )

--- lib/native/virtual_mic.c
+++ lib/native/virtual_mic.c
@@ -15,17 +15,20 @@
 // Configuartions
 static uint32_t RATE = 48000;
 static uint32_t CHANNELS = 2;
-static const char *NODE_NAME = "virtual_mic_from_c";
+static const char *NODE_MIC_NAME = "virtual_mic_from_c";
+static const char *NODE_AUDIO_NAME = "virtual_audio_from_c";
 static bool initiailized = false;
 
 // PipeWiere base functions
 static struct pw_main_loop *loop = NULL;
 static struct pw_context *context = NULL;
 static struct pw_core *core = NULL;
-static struct pw_stream *stream = NULL;
+static struct pw_stream *mic_stream = NULL;
+static struct pw_stream *audio_stream = NULL;
 static struct spa_hook stream_listener;
 static struct pw_registry *registry;
 static struct pw_thread_loop *thread_loop = NULL;
+
 double phase = 0.0;
 
 // Stream callbacks
@@ -48,24 +51,23 @@ static struct spa_pod *make_audio_format(struct spa_pod_builder *builder){
 }
 
 int wirite_frames(int16_t *frames_buf, size_t frames_count){
-  if (!stream || !initiailized) return -1;
+  if (!mic_stream || !initiailized) return -1;
   
-  struct pw_buffer *b = pw_stream_dequeue_buffer(stream);
+  struct pw_buffer *b = pw_stream_dequeue_buffer(mic_stream);
   struct spa_buffer *buf;
-
-  int i, c;
   int16_t *dst, val;
-
+  int i, c;
+  
+  buf = b->buffer;
+  dst = buf->datas[0].data;
+  
   size_t stride = CHANNELS * sizeof(int16_t);
-
+  
   if (b == NULL){
     pw_log_warn("out of buffers: %m");
     return -1;
   }
-
-  buf = b->buffer;
-  dst = buf->datas[0].data;
-
+  
   if (dst == NULL) return -1;
 
   memcpy(dst, frames_buf, frames_count * stride);
@@ -74,14 +76,19 @@ int wirite_frames(int16_t *frames_buf, size_t frames_count){
   buf->datas[0].chunk->stride = stride;
   buf->datas[0].chunk->size = frames_count * stride;
   
-  pw_stream_queue_buffer(stream, b);
+  pw_stream_queue_buffer(mic_stream, b);
 
+}
+
+/// show the data, debug only
+void audio_debug(uint8_t *data){
+  printf("buffer %d\n", *data);
 }
 
 /// Send a sin wave to microphone, debug only
 ///
 /// for see the sin wave open an recorder.
-static void on_process(void *data) {
+static void on_process_mic(void *data) {
   const size_t block = 256;
   double inc = 2*M_PI / (double)RATE;
 
@@ -102,27 +109,62 @@ static void on_process(void *data) {
   
 }
 
+/// On have audio send to listener. 
+static void on_process_audio(void *data){
+  void (*listener)(uint8_t *data) = data;
+
+  struct pw_buffer *b = pw_stream_dequeue_buffer(audio_stream);
+  struct spa_buffer *buf;
+  uint8_t *p;
+
+  if (b == NULL){
+    pw_log_warn("out of buffers: %m");
+    return;
+  }
+
+  buf = b->buffer;
+  p = buf->datas[0].data;
+
+  int stride = sizeof(float) * CHANNELS;
+  int n_frames = buf->datas[0].maxsize / stride;
+
+  if (b->requested)
+    n_frames = SPA_MIN((int)b->requested, n_frames);
+
+  buf->datas[0].chunk->offset = 0;
+  buf->datas[0].chunk->stride = stride;
+  buf->datas[0].chunk->size = n_frames * stride;
+
+  pw_stream_queue_buffer(audio_stream, b);
+
+  if (*p != 0){
+    listener(p);
+  }
+
+ 
+}
+
 // Crete one virtual mic
 int create_virtual_mic(char *name, bool debug){
-  if (stream) return 0; // already created
+  if (mic_stream) return 0; // already created
 
   struct pw_properties *props = pw_properties_new(
-        PW_KEY_NODE_NAME, "virtual-microphone",
-        PW_KEY_MEDIA_CLASS, "Audio/Source",
-        PW_KEY_MEDIA_ROLE, "virtual",
-        NULL);
+    PW_KEY_NODE_NAME, "virtual-microphone",
+    PW_KEY_MEDIA_CLASS, "Audio/Source",
+    PW_KEY_MEDIA_ROLE, "virtual",
+    NULL
+  );
 
+  mic_stream = pw_stream_new(core, name ? name : NODE_MIC_NAME, props); /// The chanel for wirite in microphone
 
-  stream = pw_stream_new(core, name ? name : NODE_NAME, props); /// The chanel for wirite in microphone
-
-  if (!stream) return -1;
+  if (!mic_stream) return -1;
 
   if (debug){ /// Enable debug funtions and logs
     static struct pw_stream_events events = { PW_VERSION_STREAM_EVENTS };
     events.state_changed = stream_state_changed; 
-    events.process = on_process; 
+    events.process = on_process_mic; 
 
-    pw_stream_add_listener(stream, &stream_listener, &events, NULL); 
+    pw_stream_add_listener(mic_stream, &stream_listener, &events, NULL); 
   }
 
   uint8_t buffer[1024];
@@ -131,24 +173,85 @@ int create_virtual_mic(char *name, bool debug){
   params[0] = make_audio_format(&builder);
 
   int res = pw_stream_connect(
-    stream,
+    mic_stream,
     PW_DIRECTION_OUTPUT, /// we provide a source: other apps read from our output
     PW_ID_ANY,
     PW_STREAM_FLAG_AUTOCONNECT | PW_STREAM_FLAG_MAP_BUFFERS | PW_STREAM_FLAG_RT_PROCESS,
     params,
     1
   );
-
-  if (debug){
-    pw_main_loop_run(loop); /// block the process, until pw_main_loop_quit() 
-  }
-  else{
-    thread_loop = pw_thread_loop_new_full(pw_main_loop_get_loop(loop), "mic_thread", NULL);
-    pw_thread_loop_start(thread_loop);
+  
+  if (thread_loop == NULL){
+    if (debug){
+      pw_main_loop_run(loop); /// block the process, until pw_main_loop_quit() 
+    }
+    else{
+      thread_loop = pw_thread_loop_new_full(pw_main_loop_get_loop(loop), "mic_thread", NULL);
+      pw_thread_loop_start(thread_loop);
+    }
   }
 
   return 0;
   
+}
+
+int create_audio_listener(char *name, bool debug, void (*listener)(uint8_t *data)){
+  if (audio_stream) return 0; // already created
+
+  struct pw_properties *props = pw_properties_new(
+    PW_KEY_NODE_NAME, "audio-listener",
+    PW_KEY_MEDIA_CATEGORY, "Capture",
+    PW_KEY_MEDIA_ROLE, "Music",
+    NULL
+  );
+
+  audio_stream = pw_stream_new(core, name ? name : NODE_AUDIO_NAME, props); /// The chanel for wirite in microphone
+
+  if (!audio_stream) return -1;
+
+  static struct pw_stream_events events = { PW_VERSION_STREAM_EVENTS };
+  events.process = on_process_audio; 
+
+  if (debug){ /// Enable logs
+    events.state_changed = stream_state_changed; 
+  }
+
+  pw_stream_add_listener(audio_stream, &stream_listener, &events, listener); 
+
+  uint8_t buffer[1024];
+
+  struct spa_pod_builder builder = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
+  const struct spa_pod *params[1];
+  
+  params[0] = spa_format_audio_raw_build(
+    &builder,
+    SPA_PARAM_EnumFormat,
+    &SPA_AUDIO_INFO_RAW_INIT(
+      .format = SPA_AUDIO_FORMAT_F32
+    )
+  );
+
+  pw_stream_connect(audio_stream,
+    PW_DIRECTION_INPUT,
+    PW_ID_ANY,
+    PW_STREAM_FLAG_AUTOCONNECT |
+    PW_STREAM_FLAG_MAP_BUFFERS |
+    PW_STREAM_FLAG_RT_PROCESS,
+    params, 1
+  );
+
+  if (thread_loop == NULL){
+    if (debug){
+      pw_main_loop_run(loop); /// block the process, until pw_main_loop_quit() 
+    }
+    else{
+      thread_loop = pw_thread_loop_new_full(pw_main_loop_get_loop(loop), "audio_thread", NULL);
+      pw_thread_loop_start(thread_loop);
+    }
+  }
+
+
+  return 0;
 }
 
 void start_pipewire(void){
@@ -172,13 +275,13 @@ void stop_pipewire(void *userdata, int signal_number){
     pw_thread_loop_destroy(thread_loop);
   }
 
-  pw_stream_destroy(stream);
+  pw_stream_destroy(mic_stream);
   pw_main_loop_destroy(loop);
   pw_context_destroy(context);
   pw_core_disconnect(core);
   pw_deinit();
   
-  stream = NULL;
+  mic_stream = NULL;
   loop = NULL;
   context = NULL;
   core = NULL;
@@ -189,7 +292,8 @@ void stop_pipewire(void *userdata, int signal_number){
 #ifdef VMIC_STANDALONE
 void main(void){
   start_pipewire();
-  create_virtual_mic(NULL, true); // allowed logs
+  create_audio_listener(NULL, true, &audio_debug);
+  // create_virtual_mic(NULL, true); // allowed logs
 }
 #endif
 // gcc -shared -fPIC -o libvirtual_mic.so virtual_mic.c $(pkg-config --cflags --libs libpipewire-0.3)
