d6e5b07a561ea19a2c85f708fdba524211c555f0
NoachDev <noachdev@proton.me>
2026-03-17 10:24:19 -0300

    update : icon, android org,  and the pubsepc dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.laughing_dollop"
+    namespace = "com.opensource.laughing_dollop"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.laughing_dollop"
+        applicationId = "com.opensource.laughing_dollop"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/opensource/laughing_dollop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.opensource.laughing_dollop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
